02422e69ea760eeda511702fb5b551f3e87975ba
Geronimo Orozco <geronimo.orozco@intel.com>
2014-03-20 17:22:58 -0600

    Add postinst script

---
+++ postinst.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+source /home/vagrant/.bashrc
+echo "Installing ruby 1.8..."
+rvm install ruby-1.8
+rvm use rvm use ruby-1.8
+echo "Installing rails..."
+gem install rails
+cd /vagrant
+echo "Clonning sample app..."
+git clone https://github.com/railstutorial/sample_app.git
+cd sample_app
+echo "Installing app..."
+bundle install
+echo "Setup project db..."
+rake db:setup
+echo "Launching rail server..."
+sudo apt-get install screen
+nohup screen rails server
+
