aaced3f8f9538cc1376a4b7dce11f3e424ad7133
Geronimo Orozco <geronimo.orozco@intel.com>
2014-03-20 18:56:32 -0600

    Add scripts post inst

---
+++ launchapp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd /vagrant/sample_app
+echo "Launching rails app..."
+/home/vagrant/.rvm/gems/ruby-1.8.7-p374/bin/rails server
--- postinst.sh
+++ postinst.sh
@@ -4,13 +4,10 @@ echo "Installing ruby 1.8..."
 rvm install ruby-1.8
 cd /vagrant
 echo "Clonning sample app..."
-#git clone https://github.com/railstutorial/sample_app.git
+git clone https://github.com/railstutorial/sample_app.git
 cd sample_app
 /home/vagrant/.rvm/bin/rvm use 1.8
 echo "Installing app..."
 /home/vagrant/.rvm/gems/ruby-1.8.7-p374@global/bin/bundle install
 echo "Setup project db..."
 /home/vagrant/.rvm/gems/ruby-1.8.7-p374@global/bin/rake db:setup
-echo "Launching rails app..."
-/home/vagrant/.rvm/gems/ruby-1.8.7-p374/bin/rails server
-
