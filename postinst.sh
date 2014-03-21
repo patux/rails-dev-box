6b82a58d5580fc4dd067eba39a764dea88a7df8a
Geronimo Orozco <geronimo.orozco@intel.com>
2014-03-20 18:47:09 -0600

    Add fixes to postinst

--- postinst.sh
+++ postinst.sh
@@ -2,18 +2,15 @@
 source /home/vagrant/.bashrc
 echo "Installing ruby 1.8..."
 rvm install ruby-1.8
-rvm use 1.8
-echo "Installing rails..."
-gem install rails
 cd /vagrant
 echo "Clonning sample app..."
-git clone https://github.com/railstutorial/sample_app.git
+#git clone https://github.com/railstutorial/sample_app.git
 cd sample_app
+/home/vagrant/.rvm/bin/rvm use 1.8
 echo "Installing app..."
-bundle install
+/home/vagrant/.rvm/gems/ruby-1.8.7-p374@global/bin/bundle install
 echo "Setup project db..."
-rake db:setup
-echo "Launching rail server..."
-sudo apt-get install screen
-nohup screen rails server
+/home/vagrant/.rvm/gems/ruby-1.8.7-p374@global/bin/rake db:setup
+echo "Launching rails app..."
+/home/vagrant/.rvm/gems/ruby-1.8.7-p374/bin/rails server
 
