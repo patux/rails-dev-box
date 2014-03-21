693f3ca0042cda2e2e8b18aa62cfcc55eceb3b2b
Geronimo Orozco <geronimo.orozco@intel.com>
2014-03-20 19:27:04 -0600

    Simplify postinst.sh

--- launchapp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /vagrant/sample_app
-echo "Launching rails app..."
-/home/vagrant/.rvm/gems/ruby-1.8.7-p374/bin/rails server
--- postinst.sh
+++ postinst.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-source /home/vagrant/.bashrc
 echo "Installing ruby 1.8..."
-rvm install ruby-1.8
+/home/vagrant/.rvm/bin/rvm install ruby-1.8
 cd /vagrant
 echo "Clonning sample app..."
 git clone https://github.com/railstutorial/sample_app.git
@@ -11,3 +10,5 @@ echo "Installing app..."
 /home/vagrant/.rvm/gems/ruby-1.8.7-p374@global/bin/bundle install
 echo "Setup project db..."
 /home/vagrant/.rvm/gems/ruby-1.8.7-p374@global/bin/rake db:setup
+echo "Launching rails app..."
+/home/vagrant/.rvm/gems/ruby-1.8.7-p374/bin/rails server
